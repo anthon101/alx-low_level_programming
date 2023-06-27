7b073905c14613d7a9d724dc5582cd996c0fe85b
anthon101 <antenehyirgalem@gmail.com>
2023-06-26 19:06:43 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * @s: string
- * return: length
+ * @s:the string to get the lenght
+ * return:the length of @str
  */
 int _strlen(char *s)
 {
